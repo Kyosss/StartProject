4725ad263713a2cf749b54c61647f67e96edc27b
Kyosss <drack.kyo@gmail.com>
2025-04-04 20:05:16 -0300

    update Player.cs movement and gravity

--- StartProjects/Assets/Scripts/Player.cs
+++ StartProjects/Assets/Scripts/Player.cs
@@ -1,16 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
+
+    public float Speed;
+    private Rigidbody2D rig;
+    public float JumpForce;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Speed * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown("Jump"))
+        {
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
