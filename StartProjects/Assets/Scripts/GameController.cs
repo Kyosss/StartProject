9bf0bba08a917d4bb1965b69770c6a891a9868b9
Kyosss <drack.kyo@gmail.com>
2025-04-08 13:14:34 -0300

    create scene GameOver and button Restart

--- StartProjects/Assets/Scripts/GameController.cs
+++ StartProjects/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameController : MonoBehaviour
@@ -9,6 +10,9 @@ public class GameController : MonoBehaviour
 
     public int totalScore;
     public Text scoreText;
+
+    public GameObject gameOver;
+
     public static GameController instance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,4 +26,14 @@ public class GameController : MonoBehaviour
         scoreText.text = totalScore.ToString();
     }
 
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
+    }
+    
 }
