101376a7861382ad9fa6de4d6fdd043c23e63b45
Kyosss <drack.kyo@gmail.com>
2025-04-11 12:11:54 -0300

    update Trampoline config

--- StartProjects/Assets/Scripts/Trampoline.cs
+++ StartProjects/Assets/Scripts/Trampoline.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    private Animator anim;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
+    
+    public float JumpForce;
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("jump");
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            collision.gameObject.GetComponent<Animator>().SetBool("jump", true);
+        }
     }
 }
